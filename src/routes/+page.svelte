d9d8a285647843369139e30dc124379135622c5e
managedhorse <david.webpersonal@gmail.com>
2025-08-03 19:21:04 +0800

    Replace logo with ‘Plink Mianus’ and update Plinko UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import grassBg from '$lib/assets/grassbg.webp';
   import logo from '$lib/assets/logo.svg';
   import Balance from '$lib/components/Balance.svelte';
   import LiveStatsWindow from '$lib/components/LiveStatsWindow/LiveStatsWindow.svelte';
@@ -18,9 +19,7 @@
 <div class="relative flex min-h-dvh w-full flex-col">
   <nav class="sticky top-0 z-10 w-full bg-gray-700 px-5 drop-shadow-lg">
     <div class="mx-auto flex h-14 max-w-7xl items-center justify-between">
-      <span class="text-2xl font-bold text-white">
-      Plink Mianus
-    </span>
+      <span class="text-2xl font-bold text-white">Plink Mianus</span>
       <div class="mx-auto">
         <Balance />
       </div>
@@ -40,11 +39,12 @@
 
   <SettingsWindow />
   <LiveStatsWindow />
-
 </div>
 
 <style>
   :global(body) {
-    @apply bg-gray-800;
+    background: url("{grassBg}") center/cover no-repeat;
+    /* ensure the page is at least full viewport height */
+    min-height: 100vh;
   }
 </style>
